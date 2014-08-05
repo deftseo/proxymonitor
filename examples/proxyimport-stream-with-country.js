ded037330ab67223d49f0c53ff5ee1279cf2df91
Bailey Reynolds <bailey@deftseo.com>
2014-08-05 11:33:06 +0000

    streaming import example with country-code lookup

---
+++ examples/proxyimport-stream-with-country.js
@@ -0,0 +1,31 @@
+var fs         = require('fs'),
+    proxyutils = require('../lib/proxyutils'),
+    proxystore = require('../lib/proxystore'),
+    dataDir    = './var/incoming/',
+    inputFile  = dataDir + 'test-list.txt';
+    //inputFile  = '/home/seo/tmp/elite-1.txt';
+
+
+// Set up each pipe Transform
+var inStream   = fs.createReadStream(inputFile),
+    lineChunk  = proxyutils.lineChunker(),
+    proxyParse = proxyutils.proxyParse(),
+    countryIp  = proxyutils.countryLookup(),
+    proxySave  = proxystore.proxySaver();
+
+// Connecting the streams.
+// inStream -> lineChunk -> proxyParse -> proxyDump
+inStream
+    .pipe(lineChunk)
+    .pipe(proxyParse)
+    .pipe(countryIp)
+    .pipe(proxySave)
+    .on('finish', function() {
+        console.log('Finished streaming');
+
+        var dbDump = proxystore.dump();
+        console.log("ProxyDB:", dbDump);
+
+        console.log("ProxyStore:", proxystore.length(), "proxies stored");
+    });
+
