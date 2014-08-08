b754b7f81e6ff2e43f935cd42dba5ef59f086aa0
Bailey Reynolds <bailey@deftseo.com>
2014-08-08 12:13:11 +0000

    Stream for importing proxies from textfiles to the proxy store

---
+++ proxyimport.js
@@ -0,0 +1,90 @@
+var fs         = require('fs'),
+    proxyutils = require('./lib/proxyutils'),
+    proxystore = require('./lib/proxystore'),
+    dataDir    = './var/incoming/',
+    inputFile  = dataDir + 'test-list.txt';
+
+
+
+var util   = require('util'),
+    stream = require('stream');
+
+function Summariser(options) {
+    if (!(this instanceof Summariser)) {
+        return new Summariser(options);
+    }
+
+    if (!options) {
+        options = {};
+    }
+
+    options.objectMode = true;
+    stream.Transform.call(this, options);
+
+    this.countryStats = {};
+}
+
+util.inherits(Summariser, stream.Transform);
+
+Summariser.prototype._transform = function(proxy, enc, done) {
+    var country = proxy.country;
+
+    if (!this.countryStats[country]) {
+        this.countryStats[country] = 0;
+    }
+    this.countryStats[country]++;
+
+    this.push(proxy);
+    done();
+}
+
+Summariser.prototype.summarise = function() {
+    var keys = Object.keys(this.countryStats),
+        total = 0,
+        i, j;
+
+    keys.sort();
+
+    for (i = 0, j = keys.length; i < j; i++) {
+        var key = keys[i],
+            value = this.countryStats[key];
+
+        console.log(key, ':', value);
+        total += value;
+    }
+
+    console.log('Total:', total);
+}
+
+
+// Set up each pipe Transform
+var inStream    = fs.createReadStream(inputFile),
+    lineChunk   = proxyutils.lineChunker(),
+    proxyParse  = proxyutils.proxyParse(),
+    proxyDedupe = proxyutils.proxyDedupe(),
+    countryIp   = proxyutils.countryLookup(),
+    summariser  = new Summariser(),
+    proxySave   = proxystore.proxySaver();
+
+console.log('Importing proxies into proxy store');
+
+// Connecting the streams.
+// inStream -> lineChunk -> proxyParse -> dupe filter -> country/ip ->  proxySave
+inStream
+    .pipe(lineChunk)
+    .pipe(proxyParse)
+    .pipe(proxyDedupe)
+    .pipe(countryIp)
+    .pipe(summariser)
+    .pipe(proxySave)
+    .on('finish', function() {
+        process.stdout.write('\n');
+        console.log('Finished streaming');
+
+        proxystore.length(function(length) {
+            console.log('ProxyStore:', length, 'proxies stored');
+        });
+
+        summariser.summarise();
+    });
+
